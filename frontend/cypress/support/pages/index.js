bac2d331bf50e59dd16923770b3194b62a11152e
Truc Vi <trucvidt2903@gmail.com>
2025-12-09 13:12:29 +0700

    chinh sua e2e

--- frontend/cypress/support/pages/index.js
+++
@@ -1,6 +0,0 @@
-import ProductsPage from './ProductsPage';
-import ProductDetailsPage from './ProductDetailsPage';
-import CartPage from './CartPage';
-import ProductPage from './ProductPage';
-
-export { ProductsPage, ProductDetailsPage, CartPage, ProductPage };
